1884bcf9d7ded9ab290a8476cb1613f780d71b32
[mahapatrasonali] <[sonangr2000@gmail.com]>
2022-12-28 13:30:43 +0530

    Create a Queue

---
+++ Queue.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureDemo
+{
+    public class Queue
+    {
+        public void QueueDisplay()
+        {
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(70);
+            queue.Enqueue(30);
+            queue.Enqueue(56);
+
+            foreach(int ele in queue)
+            {
+                Console.WriteLine(ele);
+            }
+        }
+    }
+}
